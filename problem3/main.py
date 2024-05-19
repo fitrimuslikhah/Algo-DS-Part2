f5bde7b3ebc1ce298b4307f35ed8e0556d7fc0c4
fitri <fitrimuslikhah7@gmail.com>
2024-05-19 21:01:08 +0700

    problem 3

--- problem3/main.py
+++ problem3/main.py
@@ -1,5 +1,9 @@
 def playing_domino(cards, deck):
-    
+    start, end = deck  
+    for card in cards:
+        if start in card or end in card:
+            return card  
+    return []
 
 if __name__ == "__main__":
     print(playing_domino([[6, 5], [3, 4], [2, 1], [3, 3]], [4, 3]))  # [3, 4]
